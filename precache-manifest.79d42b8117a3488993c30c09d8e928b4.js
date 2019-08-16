2981630918a1a9e1f46684106021cd884520d4fe
roopamgarg <roopamg777@gmail.com>
2019-08-16 12:00:09 +0530

    Updates

---
+++ precache-manifest.79d42b8117a3488993c30c09d8e928b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42b7cfd28c81fd8be095b0bd594671f",
+    "url": "/conveGenius/index.html"
+  },
+  {
+    "revision": "8ad0a0ea7b2d146eeefd",
+    "url": "/conveGenius/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "83557135e9d91711f67e",
+    "url": "/conveGenius/static/css/main.9eaf34d3.chunk.css"
+  },
+  {
+    "revision": "8ad0a0ea7b2d146eeefd",
+    "url": "/conveGenius/static/js/2.c64891a7.chunk.js"
+  },
+  {
+    "revision": "83557135e9d91711f67e",
+    "url": "/conveGenius/static/js/main.4d75def1.chunk.js"
+  },
+  {
+    "revision": "65500547e1179c6d8f23",
+    "url": "/conveGenius/static/js/runtime~main.981d326d.js"
+  },
+  {
+    "revision": "372e1aac992d50ba1604655402783e20",
+    "url": "/conveGenius/static/media/contactus-banner.372e1aac.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/conveGenius/static/media/logo.5d5d9eef.svg"
+  }
+]);
